366dbccc590ba9bfd600583971fe8231b2dfcf2d
katun <work55501@yandex.ru>
2018-09-28 01:58:43 +0300

    some changes and improvements

--- src/OlxFactory.php
+++ src/OlxFactory.php
@@ -3,14 +3,14 @@ namespace Parhomenko\Olx;
 
 class OlxFactory{
 
-    public static function get( $country_code, array $credentials ) {
+    public static function get( $country_code, array $credentials, bool $update_token = false ) {
 
         $countries = [
             'ua' => '\\Parhomenko\\Olx\\Ua\\Api'
         ];
 
         if ( key_exists( strtolower($country_code), $countries) ){
-            return new $countries[$country_code]( $credentials );
+            return new $countries[$country_code]( $credentials, $update_token );
         }
 
         throw new \Exception( "Country does not supported" );
--- src/Ua/User.php
+++ src/Ua/User.php
@@ -3,9 +3,11 @@ namespace Parhomenko\Olx\Ua;
 
 
 use GuzzleHttp\Client;
+use function GuzzleHttp\Psr7\build_query;
 
 class User
 {
+    const OLX_AUTH_REQUEST_URI = '/api/open/oauth/token';
     const OLX_AUTH_DEFAULT_GRAND_TYPE = 'authorization_code';
     const OLX_AUTH_DEFAULT_SCOPE = 'read write v2';
     const OLX_AUTH_DEFAULT_TOKEN_TYPE = 'bearer';
@@ -26,42 +28,77 @@ class User
         'client_secret'
     ];
 
-    private $required_auth_credentials = [
-        'access_token',
-        'refresh_token',
-        'expires_in',
-        'updated_at'
-    ];
-
     public function __construct( Client $guzzleClient, $credentials )
     {
-        $this->valideteCredentials( $credentials );
+        $this->validateCredentials( $credentials );
 
         $this->guzzleClient = $guzzleClient;
+
         $this->client_id = $credentials['client_id'];
         $this->client_secret = $credentials['client_secret'];
-
-        $this->access_token = $credentials['access_token'];
-        $this->refresh_token = $credentials['refresh_token'];
+        $this->access_token = $credentials['access_token'] ?? null;
+        $this->refresh_token = $credentials['refresh_token'] ?? null;
         $this->token_type = $credentials['token_type'] ?? self::OLX_AUTH_DEFAULT_TOKEN_TYPE;
         $this->grant_type = $credentials['grant_type'] ?? self::OLX_AUTH_DEFAULT_GRAND_TYPE;
         $this->scope = $credentials['scope'] ?? self::OLX_AUTH_DEFAULT_SCOPE;
-        $this->token_expires_in = $credentials['expires_in'];
-        $this->token_updated_at = $credentials['updated_at'];
+        $this->token_expires_in = $credentials['expires_in'] ?? 0;
+        $this->token_updated_at = $credentials['updated_at'] ?? '2000-01-01 00:00:00';
     }
 
     /**
-     * @param array $credintials
+     * @return integer
+     */
+    public function getClientId(){
+        return $this->client_id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getClientSecret(){
+        return $this->client_secret;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTokenType(){
+        return $this->token_type;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAccessToken(){
+        return $this->access_token;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRefreshToken(){
+        return $this->refresh_token;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTokenExpiresIn(){
+        return $this->token_expires_in;
+    }
+
+    /**
+     * @param array $credentials
      * @return bool
      * @throws \Exception
      */
-    private function valideteCredentials( array $credintials )
+    private function validateCredentials( array $credentials )
     {
         $missing_credentials = [];
 
         foreach ($this->required_credentials as $required_credential )
         {
-            if( !key_exists( $required_credential, $credintials ) ) $missing_credentials[] = $required_credential;
+            if( !key_exists( $required_credential, $credentials ) ) $missing_credentials[] = $required_credential;
         }
 
         if( !empty($missing_credentials) ) throw new \Exception('Missing credentials: ' .implode(', ',$missing_credentials ) );
@@ -69,15 +106,10 @@ class User
         return true;
     }
 
-    public function getTokenType(){
-        return $this->token_type;
-    }
-
-    public function getAccessToken(){
-        return $this->access_token;
-    }
-
-    public function checkToken()
+    /**
+     * Check if token is invalid or unexpected
+     */
+    public function checkToken() : self
     {
 
         if( !$this->access_token )
@@ -85,66 +117,56 @@ class User
             $this->refreshToken();
         }else{
 
-            $date_time_expiries = new \DateTime($this->token_updated_at);
-            $date_time_expiries->add( new \DateInterval( 'PT' .$this->token_expires_in .'S' ) );
+            $date_time_expires = new \DateTime( $this->token_updated_at );
+            $date_time_expires->add( new \DateInterval( 'PT' .$this->token_expires_in .'S' ) );
 
-            if( $date_time_expiries <= new \DateTime() )
+            if( $date_time_expires <= new \DateTime() )
             {
                 $this->refreshToken();
             }
 
         }
 
-    }
-
-
-    /**
-     * Log in to olx
-     */
-    public function login()
-    {
-
-        try {
-
-            $response = $this->guzzleClient->request('POST', '/api/open/oauth/token', [ 'json' => [
-                'client_id' => $this->client_id,
-                'client_secret' => $this->client_secret,
-                'grant_type' => $this->grant_type,
-                'scope' => $this->scope
-                ]
-            ]);
-
-            $data = json_decode( $response->getBody()->getContents(), true );
-
-            if( !empty($data['access_token']) ){
-                $this->token = $data['access_token'];
-                $this->token_type = $data['token_type'];
-                $this->refresh_token = $data['refresh_token'];
-                $this->token_expires_in = $data['expires_in'];
-            }else{
-                throw new \Exception( 'Can not get access token' );
-            }
-
-        } catch (\Exception $e) {
-            throw $e;
-        }
-
         return $this;
     }
 
-    public function authorize(){
+    /**
+     * Step 1. Get OAuth link
+     * @param string $redirect_uri
+     * @param string $state
+     * @return string
+     */
+    public function getOAuthLink( string $redirect_uri = '', string $state = '' ) : string
+    {
+        $params = [
+            'client_id' =>$this->client_id,
+            'response_type' => 'code',
+            'scope' => self::OLX_AUTH_DEFAULT_SCOPE,
+        ];
 
-        try {
+        if( !empty($redirect_uri) ) $params['redirect_uri'] = $redirect_uri;
+        if( !empty($state) ) $params['state'] = $state;
 
-            $response = $this->guzzleClient->request('POST', '/api/open/oauth/token', [ 'json' => [
+        return $this->guzzleClient->getConfig( 'base_uri') .'oauth/authorize/?' .build_query( $params );
+    }
+
+    /**
+     * Step2. Get access token via code
+     * @param string $code
+     * @return User
+     * @throws \Exception
+     */
+    public function authorize( string $code ) : self
+    {
+        try{
+
+            $response = $this->guzzleClient->request('POST', self::OLX_AUTH_REQUEST_URI, [ 'json' => [
                 'client_id' => $this->client_id,
                 'client_secret' => $this->client_secret,
                 'grant_type' => self::OLX_AUTH_DEFAULT_GRAND_TYPE,
                 'scope' => $this->scope,
-                //'redirect_uri' => 'http://cb69336f.ngrok.io/oauth/olx',
-                'code' => '4fb41f60d5d3efe73673a44c5ae5bcca884b42d5'
-            ]
-            ]);
+                'code' => $code
+            ] ]);
 
             $data = json_decode( $response->getBody()->getContents(), true );
 
@@ -157,17 +179,48 @@ class User
             }else{
                 throw new \Exception( 'Can not get access token' );
             }
-
-        } catch (\Exception $e) {
+        }
+        catch (\Exception $e) {
             throw $e;
         }
 
         return $this;
-
     }
 
-    public function refreshToken(){
+    /**
+     * Refresh access token via refresh_token
+     * @return User
+     * @throws \Exception
+     */
+    public function refreshToken() : self
+    {
+        try {
 
+            $response = $this->guzzleClient->request('POST', self::OLX_AUTH_REQUEST_URI, [ 'json' => [
+                'client_id' => $this->client_id,
+                'client_secret' => $this->client_secret,
+                'grant_type' => "refresh_token",
+                'refresh_token' => $this->refresh_token
+            ]
+            ]);
+
+            $data = json_decode( $response->getBody()->getContents(), true );
+
+            if( !empty($data['access_token']) ){
+                $this->access_token = $data['access_token'];
+                $this->token_type = $data['token_type'];
+                $this->refresh_token = $data['refresh_token'];
+                $this->token_expires_in = $data['expires_in'];
+                $this->token_updated_at = date( "Y-m-d H:i:s");
+            }else{
+                throw new \Exception( 'Can not refresh access token' );
+            }
+
+        } catch ( \Exception $e ) {
+            throw $e;
+        }
+
+        return $this;
     }
 
 }
